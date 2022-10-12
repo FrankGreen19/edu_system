063ef909fb64774b7d78db24ce7b2a7b2ecddeef
Bulat Ahmetshin <Bulat.A.Ahmetshin@tatar.ru>
2022-10-12 23:31:47 +0300

    Добавлена кастомная сериализация объектов

--- src/Controller/BasicController.php
+++ src/Controller/BasicController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
-use App\Module\BasicModule;
+use App\Entity\EntityInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -11,13 +15,25 @@ abstract class BasicController extends AbstractController
 {
     protected ValidatorInterface $validator;
     protected SerializerInterface $serializer;
-    protected BasicModule $module;
 
     public function __construct(ValidatorInterface $validator,
-                                SerializerInterface $serializer, BasicModule $module)
+                                SerializerInterface $serializer)
     {
         $this->validator = $validator;
         $this->serializer = $serializer;
-        $this->module = $module;
+    }
+
+    protected function serialize(EntityInterface $serializableObject): string
+    {
+        $encoder = new JsonEncoder();
+        $defaultContext = [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+                return $object->getId();
+            },
+        ];
+        $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
+        $serializer = new Serializer([$normalizer], [$encoder]);
+
+        return $serializer->serialize($serializableObject, 'json');
     }
 }
